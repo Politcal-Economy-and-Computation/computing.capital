4d2eb709c1abdf90b742a36c9f9912ae2deace95
nathan kim <nathanckim18@gmail.com>
2025-11-24 23:03:46 -0500

    the goddamn build directory lmfao

---
+++ build/app/immutable/nodes/2.qnm0IEeF.js
@@ -0,0 +1,28 @@
+import{f as k,a as _}from"../chunks/CV2NOMz2.js";import{i as F}from"../chunks/C72DnmIM.js";import{h as Y,U as T,W as X,X as q,C as c,aB as y,A as i,Y as w,Z as b,V as U,aC as j,_ as h,aD as z,ab as L}from"../chunks/Ct6bD1EQ.js";import{e as x}from"../chunks/D-rO4oda.js";import{o as C}from"../chunks/CtC8AdGa.js";import{p as A}from"../chunks/BcUNbreg.js";import{s as P}from"../chunks/CG788FwM.js";function N(a,e){return a==null?null:String(a)}function E(a,e,r,n){var s=a.__style;if(Y||s!==e){var t=N(e);(!Y||t!==a.getAttribute("style"))&&(t==null?a.removeAttribute("style"):a.style.cssText=t),a.__style=e}return n}var G=k('<img src="/franklin.png" alt="$100 bill" height="auto"/>');function v(a,e){T(e,!1);let r=A(e,"pageX",8,0),n=A(e,"pageY",8,0),s=A(e,"offset",8,0),t=y(0),o=0,m=0;const l=1500;C(()=>{o=performance.now();const u=()=>{const d=performance.now(),p=Math.floor(d-o);p>l?(o=o+l,c(t,0)):c(t,Math.max(p-s(),0)),m=requestAnimationFrame(u)};return m=requestAnimationFrame(u),()=>cancelAnimationFrame(m)}),F();var g=G();X(()=>E(g,`width: 50px; height: auto; position: absolute; top: ${n()+i(t)}px; left: ${r()-50}px`)),_(a,g),q()}var K=k("<div><!></div>");function R(a,e){T(e,!1);let r=A(e,"image",8),n=0,s=0,t=y(!1),o=y(0);const m=1500;C(()=>{n=performance.now();const u=()=>{const d=performance.now(),p=Math.floor(d-n);if(!i(t)){s=requestAnimationFrame(u);return}p>m?(n=n+m,c(o,0)):c(o,360*p/1500),s=requestAnimationFrame(u)};return s=requestAnimationFrame(u),()=>cancelAnimationFrame(s)}),F();var l=K(),g=w(l);P(g,r,()=>i(o)),b(l),x("mouseover",l,()=>{c(o,0),c(t,!0)}),x("mouseleave",l,()=>{c(o,0),c(t,!1)}),_(a,l),q()}var B=k(`<img src="/marx-head.png" class="small-picture svelte-1uha8ag" alt="A portrait of Karl Marx's head."/>`),D=k(`<!> <!> <!> <!> <!> <div class="wrapper svelte-1uha8ag"><main class="svelte-1uha8ag"><h1 class="svelte-1uha8ag">Political economy and algorithms collective</h1> <div id="marx-image" class="svelte-1uha8ag"><img src="/marx.png" class="big-picture svelte-1uha8ag" alt="A portrait of Karl Marx."/> <!></div> <p class="svelte-1uha8ag">The Political Economy and Algorithms collective, also known as Political
+      Economy and Computation or Political Economy and Computer Science, is an
+      interdisciplinary and multi-institutional academic, activist, and
+      organizer collective working at the intersection of politics, economics,
+      and computing. We started as a small reading group across the Boston
+      University and University of Michigan campuses, and are now primarily
+      based in Ann Arbor, with members from across the world.</p> <p class="svelte-1uha8ag">Our collective shares a political commitment to building a more just and
+      liberatory world, with a particular interest in understanding how
+      computing technologies are situated within the current racial capitalist
+      social order and how we might use computing technologies towards
+      liberatory ends. We like to draw on thinkers like Karl Marx, Silvia
+      Frederici, or Cedric Robinson (etc.) and most of our members tend to be
+      engaged in liberatory social movements, whether that be through workplace
+      organization (such as graduate student and faculty unionization),
+      internationalist solidarity movements (such as solidarity with Palestinian
+      liberation), tena.</p> <p class="svelte-1uha8ag">Organizers and intellectuals from all walks of life, all geographic
+      contexts, all flavors of Marxist inquiry, and all institutional or
+      non-institutional contexts are welcome to join. We ask that those who join
+      identify themselves among the political and intellectual commitments
+      outlined above.</p> <p class="svelte-1uha8ag">To join, first send us an email at <a href="mailto:political-economy-and-algorithms-requests@umich.edu">political-economy-and-algorithms-requests@umich.edu</a>, or fill out an onboarding form <a href="https://docs.google.com/forms/d/e/1FAIpQLScuyN_o_N-741zoRGouVn5SYfTh2x7TS3Wwh2jJUelAFa6xsA/viewform?usp=sf_link">here</a>. We may request to have a brief, informal conversation or email exchange
+      with you prior to onboarding. Alternatively, talk to a member you know —
+      anyone can add comrades to the group.</p> <p class="svelte-1uha8ag">We have many ways that our members can participate. The primary way to
+      engage with the collective is to attend our weekly meetings on Thursdays,
+      10-11AM EST on Zoom and in person at the University of Michigan.
+      Asynchronous participation is also possible through our listserv and
+      groupchat. Finally, we host several public-facing events a year, thanks to
+      funding from the University of Michigan through the Rackham
+      Interdisciplinary Workshop grants and MDemocracy grant.</p></main></div>`,1);function $(a){let e=y(0),r=y(0);var n=D();x("mousemove",j,f=>(c(e,f.pageX),c(r,f.pageY),f));var s=U(n);v(s,{get pageX(){return i(e)},get pageY(){return i(r)}});var t=h(s,2);v(t,{get pageX(){return i(e)},get pageY(){return i(r)},offset:300});var o=h(t,2);v(o,{get pageX(){return i(e)},get pageY(){return i(r)},offset:600});var m=h(o,2);v(m,{get pageX(){return i(e)},get pageY(){return i(r)},offset:900});var l=h(m,2);v(l,{get pageX(){return i(e)},get pageY(){return i(r)},offset:1200});var g=h(l,2),u=w(g),d=h(w(u),2);{const f=(S,W=z)=>{var M=B();X(()=>E(M,`transform: rotate(${W()}deg);`)),_(S,M)};var p=h(w(d),2);R(p,{get image(){return f}}),b(d)}L(10),b(u),b(g),_(a,n)}export{$ as component};
