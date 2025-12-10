1b05a28a9580f5078600cc6015bd60c473b2bb6d
Kevin Zheng <kyzheng@umass.edu>
2025-12-09 22:01:24 -0500

    center the div

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -6,13 +6,36 @@
             <li>Gena Kim • <a href="https://gena.kim" target="_blank" rel="noopener noreferrer">https://gena.kim</a> • University of Michigan</li>
             <li>Kevin Zheng • <a href="https://ze.vin" target="_blank" rel="noopener noreferrer">https://ze.vin</a> • University of Michigan</li>
         </ul>
-
-<!--        <p>-->
-<!--            {body}-->
-<!--        </p>-->
-<!--        <p class="attribution">-->
-<!--            &#45;&#45;{author}-->
-<!--        </p>-->
         <div class="spacer" style="height: 10rem;" />
     </main>
 </div>
+
+<style>
+    .wrapper {
+        width: 100vw;
+        display: flex;
+        place-content: center;
+    }
+
+    main {
+        padding: 1rem;
+        max-width: min(72ch, 100%);
+        display: flex;
+        flex-direction: column;
+        place-items: center;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    hr {
+        height: 0.1rem;
+        background: black;
+        width: 100%;
+    }
+
+    p {
+        margin: 0.5rem 0;
+    }
+</style>
