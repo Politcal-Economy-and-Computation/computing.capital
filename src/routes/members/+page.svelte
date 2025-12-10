73c2180713fdb486fe1acee33e7f22958f0e15cb
Kevin Zheng <kyzheng@umass.edu>
2025-12-09 21:38:58 -0500

    Update +page.svelte

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -7,12 +7,12 @@
             <li>Kevin Zheng • <a href="https://ze.vin" target="_blank" rel="noopener noreferrer">https://ze.vin</a> • University of Michigan</li>
         </ul>
 
-        <p>
-            {body}
-        </p>
-        <p class="attribution">
-            --{author}
-        </p>
+<!--        <p>-->
+<!--            {body}-->
+<!--        </p>-->
+<!--        <p class="attribution">-->
+<!--            &#45;&#45;{author}-->
+<!--        </p>-->
         <div class="spacer" style="height: 10rem;" />
     </main>
 </div>
