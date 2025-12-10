d98584942dd4a7bf7f14f6e2e11c56dad06d0894
Kevin Zheng <kyzheng@umass.edu>
2025-12-09 21:30:23 -0500

    start filling out members page

---
+++ src/routes/members/+page.svelte
@@ -0,0 +1,18 @@
+<div class="wrapper">
+    <main>
+        <h1>Political Economy and Algorithms Collective</h1>
+        <h2>Members</h2>
+        <ul>
+            <li>Gena Kim • <a href="https://gena.kim" target="_blank" rel="noopener noreferrer">https://gena.kim</a> • University of Michigan</li>
+            <li>Kevin Zheng • <a href="https://ze.vin" target="_blank" rel="noopener noreferrer">https://ze.vin</a> • University of Michigan</li>
+        </ul>
+
+        <p>
+            {body}
+        </p>
+        <p class="attribution">
+            --{author}
+        </p>
+        <div class="spacer" style="height: 10rem;" />
+    </main>
+</div>
