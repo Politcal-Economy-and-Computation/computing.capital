90393ad6eb67f51467e2e128e2cc454d0ce9277a
nathan kim <nathanckim18@gmail.com>
2025-12-16 12:38:34 -0500

    justine

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,41 +1,60 @@
 <div class="wrapper">
-    <main>
-        <h1>Political Economy and Algorithms Collective</h1>
-        <h2>Members</h2>
-        <ul>
-            <li>Gena Kim • <a href="https://gena.kim" target="_blank" rel="noopener noreferrer">https://gena.kim</a> • University of Michigan</li>
-            <li>Kevin Zheng • <a href="https://ze.vin" target="_blank" rel="noopener noreferrer">https://ze.vin</a> • University of Michigan</li>
-        </ul>
-        <div class="spacer" style="height: 10rem;" />
-    </main>
+  <main>
+    <h1>Political Economy and Algorithms Collective</h1>
+    <h2>Members</h2>
+    <ul>
+      <li>
+        Gena Kim • <a
+          href="https://gena.kim"
+          target="_blank"
+          rel="noopener noreferrer">https://gena.kim</a
+        > • University of Michigan
+      </li>
+      <li>
+        Kevin Zheng • <a
+          href="https://ze.vin"
+          target="_blank"
+          rel="noopener noreferrer">https://ze.vin</a
+        > • University of Michigan
+      </li>
+      <li>
+        Justine Zhang • <a
+          href="https://tisjune.github.io"
+          target="_blank"
+          rel="noopener noreferrer">https://tisjune.github.io</a
+        > • University of Michigan
+      </li>
+    </ul>
+    <div class="spacer" style="height: 10rem;" />
+  </main>
 </div>
 
 <style>
-    .wrapper {
-        width: 100vw;
-        display: flex;
-        place-content: center;
-    }
+  .wrapper {
+    width: 100vw;
+    display: flex;
+    place-content: center;
+  }
 
-    main {
-        padding: 1rem;
-        max-width: min(72ch, 100%);
-        display: flex;
-        flex-direction: column;
-        place-items: center;
-    }
+  main {
+    padding: 1rem;
+    max-width: min(72ch, 100%);
+    display: flex;
+    flex-direction: column;
+    place-items: center;
+  }
 
-    h1 {
-        text-align: center;
-    }
+  h1 {
+    text-align: center;
+  }
 
-    hr {
-        height: 0.1rem;
-        background: black;
-        width: 100%;
-    }
+  hr {
+    height: 0.1rem;
+    background: black;
+    width: 100%;
+  }
 
-    p {
-        margin: 0.5rem 0;
-    }
+  p {
+    margin: 0.5rem 0;
+  }
 </style>
