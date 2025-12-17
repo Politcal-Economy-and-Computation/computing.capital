3b909c14cb601ddb174b90419034c3e0ffd13219
David Gamba <ccontreras.gamba@gmail.com>
2025-12-16 22:30:32 -0500

    refactor members

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,60 +1,29 @@
-<div class="wrapper">
-  <main>
-    <h1>Political Economy and Algorithms Collective</h1>
-    <h2>Members</h2>
-    <ul>
-      <li>
-        Gena Kim • <a
-          href="https://gena.kim"
-          target="_blank"
-          rel="noopener noreferrer">https://gena.kim</a
-        > • University of Michigan
-      </li>
-      <li>
-        Kevin Zheng • <a
-          href="https://ze.vin"
-          target="_blank"
-          rel="noopener noreferrer">https://ze.vin</a
-        > • University of Michigan
-      </li>
-      <li>
-        Justine Zhang • <a
-          href="https://tisjune.github.io"
-          target="_blank"
-          rel="noopener noreferrer">https://tisjune.github.io</a
-        > • University of Michigan
-      </li>
-    </ul>
-    <div class="spacer" style="height: 10rem;" />
-  </main>
+<div class="flex flex-col items-center">
+  <h2 class="text-2xl font-bold mb-4">Members</h2>
+  <hr class="hr" />
+  
+  <ul class="space-y-2">
+    <li>
+      Gena Kim • 
+        <a href="https://gena.kim"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="anchor">https://gena.kim</a> • University of Michigan
+    </li>
+    <li>
+      Kevin Zheng • 
+        <a href="https://ze.vin"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="anchor">https://ze.vin</a> • University of Michigan
+    </li>
+    <li>
+      Justine Zhang • 
+        <a href="https://tisjune.github.io"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="anchor">https://tisjune.github.io</a> • University of Michigan
+    </li>
+  </ul>
+  <div class="h-40" />
 </div>
-
-<style>
-  .wrapper {
-    width: 100vw;
-    display: flex;
-    place-content: center;
-  }
-
-  main {
-    padding: 1rem;
-    max-width: min(72ch, 100%);
-    display: flex;
-    flex-direction: column;
-    place-items: center;
-  }
-
-  h1 {
-    text-align: center;
-  }
-
-  hr {
-    height: 0.1rem;
-    background: black;
-    width: 100%;
-  }
-
-  p {
-    margin: 0.5rem 0;
-  }
-</style>
