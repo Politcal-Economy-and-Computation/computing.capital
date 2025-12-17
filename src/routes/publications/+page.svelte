03e9b761d19312e5bc1da362a717192d897ac3d4
David Gamba <ccontreras.gamba@gmail.com>
2025-12-16 22:56:39 -0500

    updated pubs

--- src/routes/publications/+page.svelte
+++ src/routes/publications/+page.svelte
@@ -1,15 +1,22 @@
-<div class="wrapper">
-    <main>
-        <h1>Political Economy and Algorithms Collective</h1>
-        <h2>Publications</h2>
-        <ul>
-            <li> <a href="https://arxiv.org/abs/2508.08313" target="_blank" rel="noopener noreferrer">Resisting AI Solutionism through Workplace Collective Action</a>, Presented at "Resisting AI Solutionism: Where Do We Go From Here?" workshop at CHI '25</li>
-        </ul>
-        <div class="spacer" style="height: 10rem;" />
-    </main>
+<div class="flex flex-col items-center">
+  <h2 class="text-3xl font-bold mb-4">Publications</h2>
+  
+  <ul class="space-y-2 w-full">
+    <li>
+      <a 
+        href="https://arxiv.org/abs/2508.08313" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        class="anchor">
+        Resisting AI Solutionism through Workplace Collective Action
+      </a>, Presented at "Resisting AI Solutionism: Where Do We Go From Here?" workshop at CHI '25
+    </li>
+  </ul>
+  
+  <div class="h-40" />
 </div>
 
-<style>
+<!-- <style>
     .wrapper {
         width: 100vw;
         display: flex;
@@ -37,4 +44,4 @@
     p {
         margin: 0.5rem 0;
     }
-</style>
+</style> -->
