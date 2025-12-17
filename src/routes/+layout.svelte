ade5e1494066151d64763a0e45a354f26495904f
David Gamba <ccontreras.gamba@gmail.com>
2025-12-17 00:13:07 -0500

    improved nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import { AppBar } from '@skeletonlabs/skeleton-svelte';
   import { Moon, Sun } from '@lucide/svelte';
   import { browser } from '$app/environment';
+  import { page } from '$app/stores';
   
   let { children } = $props();
   
@@ -29,6 +30,9 @@
     }
   }
 
+  function isActive(path: string) {
+    return $page.url.pathname === path;
+  }
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
@@ -39,14 +43,32 @@
       <AppBar.Lead>
         <p class="text-xl text-secondary-50 my-0">Political Economy + Algorithms</p>
       </AppBar.Lead>
-
       <AppBar.Trail class="justify-end">
         <nav class="flex gap-6 items-center">
-          <a href="/" class="anchor text-secondary-50">Home</a>
-          <a href="/members" class="anchor text-secondary-50">Members</a>
-          <a href="/conference" class="anchor text-secondary-50">May 2026 Conference</a>
-          <a href="/publications" class="anchor text-secondary-50">Publications and Projects</a>
-          
+          <a 
+            href="/" 
+            class="anchor text-primary-100 {isActive('/') ? 'underline text-secondary-50' : ''}"
+          >
+            Home
+          </a>
+          <a 
+            href="/members" 
+            class="anchor text-primary-100 {isActive('/members') ? 'underline text-secondary-50' : ''}"
+          >
+            Members
+          </a>
+          <a 
+            href="/conference" 
+            class="anchor text-primary-100 {isActive('/conference') ? 'underline text-secondary-50' : ''}"
+          >
+            May 2026 Conference
+          </a>
+          <a 
+            href="/publications" 
+            class="anchor text-primary-100 {isActive('/publications') ? 'underline text-secondary-50' : ''}"
+          >
+            Publications and Projects
+          </a>
           <button 
             onclick={toggleTheme}
             class="btn-icon btn-icon-sm hover:preset-tonal text-secondary-50"
@@ -62,6 +84,7 @@
       </AppBar.Trail>
     </AppBar.Toolbar>
   </AppBar>
+
 
   <main class="flex-1 w-full max-w-3xl mx-auto px-4 mt-8">
     {@render children()}
