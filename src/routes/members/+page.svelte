3ed0232c1a149dd230edf4ae4db10b561318d4a2
David Gamba <ccontreras.gamba@gmail.com>
2025-12-17 01:34:27 -0500

    edit members with json

--- src/routes/members/+page.svelte
+++ src/routes/members/+page.svelte
@@ -1,24 +1,27 @@
+<script>
+  import { onMount } from 'svelte';
+  
+  /**
+     * @type {any[] | null | undefined}
+     */
+  let members = $state([]);
+  
+  onMount(async () => {
+    const response = await fetch('/data/members.json');
+    members = await response.json();
+  });
+</script>
+
 <div class="flex flex-col items-center">
   <h2>Members</h2>
-
   <ul>
-    <li>
-      Gena Kim • 
-        <a href="https://gena.kim"
-        target="_blank"
-        rel="noopener noreferrer">https://gena.kim</a> • University of Michigan
-    </li>
-    <li>
-      Kevin Zheng • 
-        <a href="https://ze.vin"
-        target="_blank"
-        rel="noopener noreferrer">https://ze.vin</a> • University of Michigan
-    </li>
-    <li>
-      Justine Zhang • 
-        <a href="https://tisjune.github.io"
-        target="_blank"
-        rel="noopener noreferrer">https://tisjune.github.io</a> • University of Michigan
-    </li>
+    {#each members as member}
+      <li>
+        {member.name} • 
+        <a href={member.url} target="_blank" rel="noopener noreferrer">
+          {member.url}
+        </a> • {member.institution}
+      </li>
+    {/each}
   </ul>
 </div>
